18134ea27f5c66d3a724d1319986b1e0a314f5e4
Grey Kramer <greykramer@gmail.com>
2024-02-08 20:12:45 -0500

    Angular17 looping options

--- Angular17-LoopFunctions/src/app/app.component.ts
+++ Angular17-LoopFunctions/src/app/app.component.ts
@@ -1,13 +1,53 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CourseCardComponent } from './course-card/course-card.component';
+import { CommonModule } from '@angular/common';
+
+export interface Course {
+  id: number;
+  description: string;
+  longDescription: string;
+  category: string;
+  lessonCount?: number;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CourseCardComponent, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Angular17-LoopFunctions';
+  courses: Course[] = [
+    {
+      id: 1,
+      description: 'Angular Core Course',
+      longDescription:
+        'A detailed walk through of angular and its core features',
+      category: 'Intermediate',
+      lessonCount: 5,
+    },
+    {
+      id: 2,
+      description: 'RXJS Practice Course',
+      longDescription: 'Understand rxjs observables and operators',
+      category: 'Beginner',
+      lessonCount: 10,
+    },
+    {
+      id: 3,
+      description: 'Angular for Beginners',
+      longDescription:
+        'Learn Angular funcdementals and learn what goes on under Angulars hood',
+      category: 'Beginner',
+      lessonCount: 12,
+    },
+  ];
+  // creating our own tracking funciton
+  // the pourpose of this funciton is to create a unique identifier
+  trackCourse(index: number, course: Course) {
+    return course.id;
+  }
 }
--- Angular17-LoopFunctions/src/app/course-card/course-card.component.ts
+++ Angular17-LoopFunctions/src/app/course-card/course-card.component.ts
@@ -1,43 +1,17 @@
-import { Component } from '@angular/core';
-
-export interface course {
-  id: number;
-  description: string;
-  longDescription: string;
-  category: string;
-  lessonCount?: number;
-}
+import { Component, Input } from '@angular/core';
+import { Course } from '../app.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-course-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './course-card.component.html',
   styleUrl: './course-card.component.scss',
 })
 export class CourseCardComponent {
-  coursesArray: course[] = [
-    {
-      id: 1,
-      description: 'Angular Core Course',
-      longDescription:
-        'A detailed walk through of angular and its core features',
-      category: 'Intermediate',
-    },
-    {
-      id: 2,
-      description: 'RXJS Practice Course',
-      longDescription: 'Understand rxjs observables and operators',
-      category: 'Beginner',
-      lessonCount: 10,
-    },
-    {
-      id: 3,
-      description: 'Angular for Beginners',
-      longDescription:
-        'Learn Angular funcdementals and learn what goes on under Angulars hood',
-      category: 'Beginner',
-      lessonCount: 12,
-    },
-  ];
+  @Input()
+  course!: Course;
+  @Input({ required: true })
+  index!: number;
 }
